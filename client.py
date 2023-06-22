ebfddf5923daa9939f5916d58ea5d7025b5bd555
Chahoud1 <roger_chahoud@outlook.com>
2023-06-22 17:03:21 -0300

    Mudança de protocolo de TCP para UDP

--- client.py
+++ client.py
@@ -1,44 +1,65 @@
+#client
 import socket
 import threading
 import time
+import random
 
-def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client.connect(('localhost', 7777))
-    except:
-        return print('\nNão foi possível se conectar ao servidor')
+# numero de processos
+num_of_procs = 4
+# numero de requisicoes de cada processo
+num_of_reqs = 1
+# sera num_of_procs*num_of_reqs, ou seja, o número total de solicitações ao servidor
+total_of_reqs = []
 
-    process_name = input('Processo - ').encode('utf-8')
-    client.send(process_name)
+# criacao do client
+# AF_INET: IPv4 // SOCKDGRAM: UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# endereco do servidor, tem que ser o mesmo para haver a conexao
+server_address = ('localhost', 7777)
 
-    threadR = threading.Thread(target=receiveMessage, args=[client])
-    threadS = threading.Thread(target=sendMessage, args=[client, process_name])
+def main(process_n):
 
-    threadR.start()
+    # thread para receber as mensagens do servidor
+    threadR = threading.Thread(target=receiveMessage, args=[])
+    # thread para enviar as mensagens ao servidor
+    threadS = threading.Thread(target=sendMessage, args=[process_n])
+
     threadS.start()
-
-
-# Envio de dados para o servidor
-def sendMessage(client, process_name):
+    threadR.start()
+# envio de dados para o servidor
+def sendMessage(process_n):
+    for i in range(num_of_reqs):
+            time.sleep(4)
+            try:
+                # tipo mensagem(1 para request)|numero do processo|complemento - mensagem
+                message = f'1|{process_n}|000000 - Preciso de acesso a regiao critica'
+                # envia bytes ao servidor
+                client.sendto(message.encode('utf-8'), server_address)
+                print(f'Messagem enviada para o servidor pelo processo {process_n}')
+            except:
+                print('erro em sendMessage')
+    return
+            
+# recebendo a resposta do servidor
+def receiveMessage():
     while True:
+        time.sleep(2)
         try:
-            client.send(f'Preciso de acesso à região crítica'.encode('utf-8'))
-            response = client.recv(1024).decode('utf-8')
-            #print(f"Resposta do servidor: {response}\n")
-            time.sleep(2)
-        except:
-            print('erro em sendMessage')
-            pass
+            # resposta do servidor
+            response, address = client.recvfrom(1024)
+            print(f"Resposta do servidor: {response.decode('utf-8')} \n")
+        except Exception as e:
+            print(f'\nSem resposta para receber do servidor')
 
-# Recebendo a resposta do servidor
-def receiveMessage(client):
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            print(f"Resposta do servidor: {message} \n")
-        except:
-            print('\nNão foi possível permanecer conectado no servidor')
-            break
+# inicia os processos
+for process_n in range(num_of_procs):
+    for req in range(num_of_reqs):
+        total_of_reqs.append(process_n)
 
-main()
+# coloca as solicitacoes dos processos em ordem aleatoria
+shuffled_total_of_reqs = random.sample(total_of_reqs, len(total_of_reqs))
+
+# começa a lancar as solicitacoes do lado do client com ordem aleatoria de processo
+for request in shuffled_total_of_reqs:
+    time.sleep(1)
+    main(request)
--- server.py
+++ server.py
@@ -1,79 +1,136 @@
-# server
 import socket
 import threading
 import time
+import datetime
+import sys
 
-clients = []
+# tempo dentro da região critica
+time_delay = 5 
+# list que contém os clientes que solicitaram conexão
+client_buffer = []
+# lock para garantir exclusão mútua no client_buffer
 clients_lock = threading.Lock()
-buffer = []
+
+# criacao do servidor
+# AF_INET: IPv4 // SOCKDGRAM: UDP
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# dominio e porta do servidor
+server_address = ('localhost', 7777)
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server.bind(('localhost', 7777))
-        server.listen()
+        # amarracao do servidor ao endereco criado
+        server.bind(server_address)
         print('Aguardando conexões...')
     except:
-        return print('\nNão foi possível iniciar o servidor!')
+        print('\nNão foi possível iniciar o servidor!')
 
+    # thread do terminal
+    thread_terminal = threading.Thread(target=terminal)
+    thread_terminal.start()
+
+    # thread da região crítica
+    thread_CR = threading.Thread(target=enterCriticalRegion)
+    thread_CR.start()
+
+    # thread para receber as conexões do client
+    thread_R_MSG = threading.Thread(target=receiveMessage)
+    thread_R_MSG.start()
+
+# grant do processo
+def enterCriticalRegion():
     while True:
-        client, address = server.accept()
-        process_name = client.recv(1024).decode('utf-8')
-        clients.append({'name': process_name, 'socket': client })
+        time.sleep(2)
+        if len(client_buffer) > 0:
+            # trava o acesso ao client_buffer
+            clients_lock.acquire()
+            # tempo dentro da regiao critica
+            time.sleep(time_delay)
+            # horario de acesso do processo a CR para ser gravado no .txt
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            # abre a escrita do .txt
+            with open("resultado.txt", "a") as file:
+                file.write(f"{timestamp} - Processo {client_buffer[0]['process_n']}\n")
+            # envia a resposta do processo
+            sendMessage(f"(GRANT) O processo {client_buffer[0]['process_n']} acessou a região crítica", client_buffer[0]['address'])
 
-        thread_messages = threading.Thread(target=messagesTreatment, args=[client, process_name])
-        thread_terminal = threading.Thread(target=terminal)
-        thread_cr = threading.Thread(target=enterCriticalRegion, args=[client, process_name])     
+            # tinha sido retirado por conta do loopback, mas parou de acontecer
+            print(f"(GRANT) O processo {client_buffer[0]['process_n']} entrou na região crítica\n")
 
-        thread_messages.start()
-        thread_terminal.start()
-        thread_cr.start()
+            # libera a escrita do client_buffer para o próximo processo
+            clients_lock.release()
+            # o processo e removido da lista
+            deleteClient()
+        else:
+            print('\nNenhum processo na fila\n')
 
-def messagesTreatment(client, process_name):
+# request do processo
+def receiveMessage():
     while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            #processo solicita entrada na região crítica
-            buffer.append(process_name)
-            print(f'{process_name}: {message}')
-        except:
-            print('erro em messagesTreatment')
-            break
+        message, address = server.recvfrom(1024)
+        # mensagem binaria para string
+        decoded_message = message.decode('utf-8')
+        # para saber qual e o processo e o que ele solicitou
+        message_parts = decoded_message.split('|')
+        if len(message_parts) >= 2:
+            # evitar condicao de corrida no client_buffer
+            with clients_lock:
+                # request do processo e armazenada no buffer
+                client_buffer.append({'message': decoded_message, 'address': address, 'process_n': message_parts[1]})
+                sendMessage(f"(REQUEST) solicitacao recebida de {message_parts[1]}", address)
+            
+                # tinha sido retirado por conta do loopback
+                print(f"(REQUEST) solicitacao recebido de {message_parts[1]}")
 
-def enterCriticalRegion(client, proccess_name):
-    while True:    
-        time.sleep(4)
-        clients_lock.acquire()
-        print(f'O processo {proccess_name} entrou na região crítica')
-        sendMessage(client, "Você acessou a região crítica")
-        if len(buffer) > 0:
-            buffer.pop(0)
-        else: 
-            print("buffer está vazio")
-        clients_lock.release()
-        time.sleep(3)
-        
+        # se cair aqui, significa que a mensagem e a propria resposta do servidor, provavel loopback
+        else:
+            print("Servidor:", decoded_message)
 
-def sendMessage(client, message):
+
+def sendMessage(message, address):
     try:
-        client.send(message.encode('utf-8'))
-    except:
-        print('Erro ao enviar mensagem')
+        # envia a mensagem apenas para o processo em questao
+        server.sendto(message.encode('utf-8'), ('localhost', address[1]))
+    except Exception as e:
+        print('Erro em sendMessage:', str(e))
+
+
+# release do processo
+def deleteClient():
+    if len(client_buffer) > 0:
+        with clients_lock:
+            sendMessage(f"(RELEASE) processo {client_buffer[0]['process_n']} removido da fila", client_buffer[0]['address'])
+            print(f"(RELEASE) Processo {client_buffer[0]['process_n']} removido da fila")
+            client_buffer.pop(0)
+    else:
+        print("buffer está vazio")
+
 
 def terminal():
     while True:
-        choice = input('\n1-Imprimir a fila de pedidos\n2-Imprimir quantas vezes cada processo foi atendido\n3-Encerrar a execução\n\n')
+        choice = input('\n1-Imprimir a fila de pedidos\n2-Imprimir quantas vezes cada processo foi atendido\n3-Encerrar a execução\n')
         if choice == '1':
-            for index, process in enumerate(buffer):
-                print(f'{index} - {process}\n')
-        if choice == '2':
-            pass
-        if choice == '3':
-            pass
+            print(client_buffer)
 
-def deleteClient(client):
-    with clients_lock:
-        if client in clients:
-            clients.remove(client)
+        if choice == '2':
+            # Dicionário para armazenar a contagem de linhas para cada número
+            contador = {}
+            with open("resultado.txt", "r") as f:
+                for line in f:
+                    number = line.strip()[-1]  # Obtém o último caractere da linha
+                    if number.isdigit():  # Verifica se é um dígito
+                        if number in contador:
+                            contador[number] += 1
+                        else:
+                            contador[number] = 1
+            # Imprime o resultado
+            for number, count in contador.items():
+                print(f"Número {number}: {count} linhas")
+
+        # encerra todas as execuções do servidor
+        if choice == '3':
+            server.shutdown(socket.SHUT_RDWR)  # Encerra leitura e escrita do socket
+            server.close()  # Fecha o socket do servidor
+            sys.exit(0)  # Encerra o programa com código de saída 0
 
 main()
